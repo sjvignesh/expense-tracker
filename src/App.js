665e4cefb6b53289eac06e20d4b0627afd1596a4
sjvignesh <vigneshj.official@gmail.com>
2023-03-01 06:01:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './App.css';
+import Expenses from './components/expenses/Expenses';
+import NewExpense from './components/new_expense/NewExpense';
+
+const DUMMY_DATA = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_DATA);
+  const onAddNewExpense = (new_expense) => {
+    setExpenses((previous_expenses) => {
+      return [new_expense, ...previous_expenses];
+    })
+  }
+  return (
+    <div className="App">
+       <NewExpense onAddNewExpense={onAddNewExpense} />
+       <Expenses expenses={expenses} />
+    </div>
+  );
+}
+
+export default App;
